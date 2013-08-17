aed0eb2ad74ab9c5a7b9bebb7bfb87eb502d4f7c
Natalia <npvc@cin.ufpe.br>
2013-08-17 16:27:23 -0300

    logar :DDD e bd

--- core/models.py
+++ core/models.py
@@ -4,21 +4,40 @@ from django.db import models
 class Restaurante(models.Model):
 	nome = models.CharField(max_length=100)
 	TIPO = (
-        ('CT','Contemporanea'),
-        ('IT','Italiana'),
-        ('JP','Japonesa'),
+        ('Contemporanea','Contemporanea'),
+        ('Italiano','Italiano'),
+        ('Japones','Japones'),
+        ('Fast food', 'Fast food'),
+        ('Pizzaria', 'Pizzaria'),
+        ('Chines', 'Chines'),
+        ('Sorveteria', 'Sorveteria'),
+        ('Doceria', 'Doceria'),
+        ('Creperia', 'Creperia')
     )
-	tipo = models.CharField(max_length=2, choices=TIPO)
+	tipo = models.CharField(max_length=20, choices=TIPO)
 	descricao = models.TextField()
 	nota_media = models.FloatField()
 	preco_medio = models.FloatField()
-	horario_funcionamento = models.TimeField()
 	site = models.CharField(max_length=100)
 	data_cadastro = models.DateTimeField(auto_now_add=True)
+	
 	# foto = models.ImageField() # tem que instalar o PIL
 	#horario_funcionamento = models.DateTimeField()
-	#seg_abertura = models.TimeField()
-	#seg_fechamento = models.TimeField()
+	dom_abertura = models.TimeField()
+	dom_fechamento = models.TimeField()
+	seg_abertura = models.TimeField()
+	seg_fechamento = models.TimeField()
+	ter_abertura = models.TimeField()
+	ter_fechamento = models.TimeField()
+	qua_abertura = models.TimeField()
+	qua_fechamento = models.TimeField()
+	qui_abertura = models.TimeField()
+	qui_fechamento = models.TimeField()
+	sex_abertura = models.TimeField()
+	sex_fechamento = models.TimeField()
+	sab_abertura = models.TimeField()
+	sab_fechamento = models.TimeField()
+
 
 	def __unicode__(self):
 		return self.nome
@@ -33,7 +52,22 @@ class Evento(models.Model):
 	descricao = models.TextField()
 	nota_media = models.FloatField()
 	preco_medio = models.FloatField()
-	horario_funcionamento = models.TimeField()
+	#horario_funcionamento = models.TimeField()
+	dom_abertura = models.TimeField()
+	dom_fechamento = models.TimeField()
+	seg_abertura = models.TimeField()
+	seg_fechamento = models.TimeField()
+	ter_abertura = models.TimeField()
+	ter_fechamento = models.TimeField()
+	qua_abertura = models.TimeField()
+	qua_fechamento = models.TimeField()
+	qui_abertura = models.TimeField()
+	qui_fechamento = models.TimeField()
+	sex_abertura = models.TimeField()
+	sex_fechamento = models.TimeField()
+	sab_abertura = models.TimeField()
+	sab_fechamento = models.TimeField()
+	
 	site = models.CharField(max_length=100)
 	data_cadastro = models.DateTimeField(auto_now_add=True)
 
