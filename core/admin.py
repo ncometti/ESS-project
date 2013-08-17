d1e388e863f7bc8d12e3b17d369511e3f333362e
Natalia <npvc@cin.ufpe.br>
2013-08-17 12:21:46 -0300

    coisando o banco

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from models import Restaurante, Comentario
+from models import Restaurante, Comentario, Evento, Diversao
 
 admin.site.register(Restaurante)
 admin.site.register(Comentario)
+admin.site.register(Evento)
+admin.site.register(Diversao)
--- core/models.py
+++ core/models.py
@@ -3,25 +3,61 @@ from django.db import models
 # Create your models here.
 class Restaurante(models.Model):
 	nome = models.CharField(max_length=100)
-	descricao = models.TextField()
 	TIPO = (
         ('CT','Contemporanea'),
         ('IT','Italiana'),
         ('JP','Japonesa'),
     )
 	tipo = models.CharField(max_length=2, choices=TIPO)
-	# foto = models.ImageField() # tem que instalar o PIL
+	descricao = models.TextField()
+	nota_media = models.FloatField()
 	preco_medio = models.FloatField()
-	qualidade = models.FloatField()
-
+	horario_funcionamento = models.TimeField()
+	site = models.CharField(max_length=100)
 	data_cadastro = models.DateTimeField(auto_now_add=True)
-	# horario_funcionamento = models.DateTimeField()
+	# foto = models.ImageField() # tem que instalar o PIL
+	#horario_funcionamento = models.DateTimeField()
 	#seg_abertura = models.TimeField()
 	#seg_fechamento = models.TimeField()
 
 	def __unicode__(self):
 		return self.nome
 
+class Evento(models.Model):
+	nome = models.CharField(max_length=100)
+	TIPO = (
+        ('Shows','Shows'),
+        ('Teatros','Teatros'),
+        ('Boates','Boates'),
+    )
+	tipo = models.CharField(max_length=15, choices=TIPO)
+	descricao = models.TextField()
+	nota_media = models.FloatField()
+	preco_medio = models.FloatField()
+	horario_funcionamento = models.TimeField()
+	site = models.CharField(max_length=100)
+	data_cadastro = models.DateTimeField(auto_now_add=True)
+
+	def __unicode__(self):
+		return self.nome
+
+class Diversao(models.Model):
+	nome = models.CharField(max_length=100)
+	TIPO = (
+        ('Museus', 'Museus'),
+        ('+Conhecidos','+Conhecidos'),
+        ('Boates','Boates'),
+    )
+	tipo = models.CharField(max_length=15, choices=TIPO)
+	descricao = models.TextField()
+	nota_media = models.FloatField()
+	preco_medio = models.FloatField()
+	horario_funcionamento = models.TimeField()
+	site = models.CharField(max_length=100)
+	data_cadastro = models.DateTimeField(auto_now_add=True)
+
+	def __unicode__(self):
+		return self.nome
 
 class Comentario(models.Model):
 	autor = models.CharField(max_length=100)
